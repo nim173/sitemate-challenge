3723059d91f450323dee1c85c1a73ea1250df88a
Nimesh De Silva <nchathuranga173@gmail.com>
2024-06-21 12:19:28 +1000

    init frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,13 +9,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Routes
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
 // Start the server
 const server = http.createServer(app)
-const port = process.env.PORT || 3000
+const port = process.env.PORT ?? 5000
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
